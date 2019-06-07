2fea609093e1c54072f4cb96f7cc3ba979761366
pedro17amador <pedro.r.amador98@gmail.com>
2019-06-07 20:19:51 -0300

    ProjetoAula-18_04_2019

---
+++ src/aula/Execucao.java
@@ -0,0 +1,12 @@
+package aula;
+
+import exec.Pessoa;
+	
+public class Execucao {
+		
+			public static void main(String[] args) {
+				// TODO Auto-generated method stub
+				Pessoa ps = new Pessoa();
+				ps.andar();
+			}
+	}
